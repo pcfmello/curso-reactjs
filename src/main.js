325acccaff2de7f2f0199a3c65a27609891ecb41
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-06-29 23:14:53 -0300

    Criado componente lista de pessoas
    Foi criado um componente para exibir uma tabela com dados de uma determinada lista, exemplificando a transformação de uma tabela em um componente.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+let listaDePessoas = [
+    {
+        id: 1,
+        name: 'Paulo Cesar',
+        email: 'paulo@javascript.floripa.br',
+        subject: 'react',
+        message: 'Estou cursando ReactJS!'
+    },
+    {
+        id: 2,
+        name: 'Claudia Rodrigues',
+        email: 'claudia@javascript.floripa.br',
+        subject: 'jquery',
+        message: 'Estou cursando ReactJS!'
+    },
+    {
+        id: 3,
+        name: 'Lucas Rodrigues',
+        email: 'lucas@javascript.floripa.br',
+        subject: 'react',
+        message: 'Estou cursando ReactJS!'
+    },
+    {
+        id: 4,
+        name: 'Nerci Rodrigues',
+        email: 'nerci@javascript.floripa.br',
+        subject: 'css',
+        message: 'Estou cursando ReactJS!'
+    }
+];
+
 let Page = React.createClass({
     render: function() {
         return (
@@ -11,7 +42,9 @@ let Page = React.createClass({
                         <Form />
                         <Button textoAtivo="Carregando...">Enviar</Button>
                     </div>
-
+                    <div className="row">
+                        <List lista={ listaDePessoas } />
+                    </div>
                 </div>
             </myElement>
         );
